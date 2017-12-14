b2bdf9160db742d909e60a3cca1a36749aebd3cf
davidbreckx <david.breckx@telenet.be>
2017-12-14 15:04:35 +0100

    Added data functionality

---
+++ Assets/Scripts/EarthDataView.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+using HoloToolkit.Unity;
+
+public class EarthDataView : MonoBehaviour {
+
+    public Text planetTitle;
+    public Text descriptionText;
+    private TextToSpeech textToSpeech;
+    private Planet planet;
+    private Transform originalParent;
+
+    public string url = "https://en.wikipedia.org/api/rest_v1/page/summary/Earth";
+
+
+    private void Awake()
+    {
+        var textToSpeechManager = GameObject.Find("TextToSpeechManager");
+        textToSpeech = textToSpeechManager.GetComponent<TextToSpeech>();
+        textToSpeech.Voice = TextToSpeechVoice.Zira;
+        //textToSpeech.runInEditMode = true;
+    }
+
+    // Use this for initialization
+    IEnumerator Start () {
+
+
+        if (IntroductionFlow.Instance != null)
+        {
+            planetTitle.text = "";
+            descriptionText.text = "";
+
+        }
+        else
+        {
+            WWW www = new WWW(url);
+
+            yield return www;
+
+            planet = JsonUtility.FromJson<Planet>(www.text);
+
+            planetTitle.text = planet.displaytitle;
+            descriptionText.text = planet.extract;
+        }
+
+        textToSpeech.StartSpeaking(descriptionText.text);
+
+        if (textToSpeech.IsSpeaking())
+        {
+            Debug.Log("Speaking");
+        }
+        else
+        {
+            Debug.Log("Not speaking");
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+    }
+
+
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class Planet
+    {
+
+        public string title;
+        public string displaytitle;
+        private int pageid;
+        public string extract;
+        private string extract_html;
+        private string thumbnail;
+        private string originalimage;
+        private string lang;
+        private string dir;
+        private string timestamp;
+        private string description;
+
+
+    }
+}
